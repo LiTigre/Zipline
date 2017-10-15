526022cc935e408520c444ec825b484bdcbd7b44
LiTigre <li.zhang17@mail.mcgill.ca>
2017-10-14 20:26:25 -0400

    navigation from team 10

--- src/ca/mcgill/ecse211/localizationlab/Navigation.java
+++ src/ca/mcgill/ecse211/localizationlab/Navigation.java
@@ -14,6 +14,7 @@ public class Navigation {
 
 	private static final int FORWARD_SPEED = 50;
 	private static final int ROTATE_SPEED = 50;
+	private static final int ERROR = 3;
 	public EV3LargeRegulatedMotor leftMotor;
 	public EV3LargeRegulatedMotor rightMotor;
 	double amountTurned;
@@ -42,45 +43,145 @@ public class Navigation {
 	}
 
 	void travelTo(double x, double y) {
+		double travelX = x - odometer.getX();
+		double travelY = y - odometer.getY();
+		double travelTotal = Math.sqrt(Math.pow(travelX, 2) + Math.pow(travelY, 2));
+		turnTo(absoluteAngle(travelX, travelY));
+		drive(travelTotal);
+		
 
-		navigating = true;
-		double deltaY = y - odometer.getY();
-		double deltaX = x - odometer.getX();
-
-		double thetaD = Math.toDegrees(Math.atan2(deltaX, deltaY));
-		double thetaTurn = thetaD - odometer.getTheta();
-		if (thetaTurn < -180.0) {
-			turnTo(360.0 + thetaTurn);
-
-		}
-		else if (thetaTurn > 180.0) {
-			turnTo(thetaTurn - 360.0);
-		}
-		else {
-			turnTo(thetaTurn);
-		}
-		while (leftMotor.isMoving() && rightMotor.isMoving()) {
-		}
-		leftMotor.setSpeed(FORWARD_SPEED);
-		rightMotor.setSpeed(FORWARD_SPEED);
-		double distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-		leftMotor.rotate(convertDistance(radius, distance), true);
-		rightMotor.rotate(convertDistance(radius, distance), false);
-
+//		navigating = true;
+//		double deltaY = y - odometer.getY();
+//		double deltaX = x - odometer.getX();
+//
+//		double thetaD = Math.toDegrees(Math.atan2(deltaX, deltaY));
+//		double thetaTurn = thetaD - odometer.getTheta();
+//		if (thetaTurn < -180.0) {
+//			turnTo(360.0 + thetaTurn);
+//
+//		}
+//		else if (thetaTurn > 180.0) {
+//			turnTo(thetaTurn - 360.0);
+//		}
+//		else {
+//			turnTo(thetaTurn);
+//		}
+//		while (leftMotor.isMoving() && rightMotor.isMoving()) {
+//		}
+//		leftMotor.setSpeed(FORWARD_SPEED);
+//		rightMotor.setSpeed(FORWARD_SPEED);
+//		double distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
+//		leftMotor.rotate(convertDistance(radius, distance), true);
+//		rightMotor.rotate(convertDistance(radius, distance), false);
 	}
 
+	
+//	/**
+//	* @param theta
+//	* finds what the robot should rotate at.
+//	*/
+//
+//	// TODO:DOES NOT WORK IN THREAD --> FIGURE THAT OUT
+//	void turnTo(double theta) {
+//
+//		leftMotor.setSpeed(ROTATE_SPEED);
+//		rightMotor.setSpeed(ROTATE_SPEED);
+//		leftMotor.rotate(convertAngle(radius, width, theta), true);
+//		rightMotor.rotate(-convertAngle(radius, width, theta), true);
+//	}
+	
 	/**
-	* @param theta
-	* finds what the robot should rotate at.
-	*/
-
-	// TODO:DOES NOT WORK IN THREAD --> FIGURE THAT OUT
-	void turnTo(double theta) {
+	 * given a distance to travel, travel straight for that distance
+	 * @param travelDist
+	 */
+	public void drive(double travelDist) {
+		for (EV3LargeRegulatedMotor motor : new EV3LargeRegulatedMotor[] { leftMotor, rightMotor }) {
+			motor.stop();
+			motor.setAcceleration(3000);
+		}
 
 		leftMotor.setSpeed(ROTATE_SPEED);
 		rightMotor.setSpeed(ROTATE_SPEED);
-		leftMotor.rotate(convertAngle(radius, width, theta), true);
-		rightMotor.rotate(-convertAngle(radius, width, theta), true);
+		leftMotor.rotate(convertDistance(radius, (int) (travelDist)), true);
+		rightMotor.rotate(convertDistance(radius, (int) (travelDist)), true);
+	}
+	
+	/**
+	 * given an absolute theta, turn to that angle
+	 */
+	public void turnTo(double theta) { // ABSOLUTE angle
+		double currentTheta = odometer.getTheta(); // theta of the robot in DEGREES
+		double turnTheta = distance(currentTheta, theta);
+		boolean crossover = false;
+		boolean clockwise = false;
+
+		double range = currentTheta + 180;
+		if (range > 360) {
+			range = range - 360;
+			crossover = true; // range passes over 0 degrees
+		}
+
+		if (crossover) { // crossover -> currentTheta between 180 and 360
+			if (theta > currentTheta || theta < range) { // in the 180 degrees counterclockwise
+				clockwise = true;
+			}
+		}
+		else { // currentTheta between 0 and 180
+			if (theta < range && theta > currentTheta) { // in the 180 degrees clockwise
+				clockwise = true;
+			}
+		}
+
+		leftMotor.setSpeed(ROTATE_SPEED);
+		rightMotor.setSpeed(ROTATE_SPEED);
+
+		if (clockwise) {
+			leftMotor.rotate(convertAngle(radius, width, turnTheta), true);
+			rightMotor.rotate(-convertAngle(radius, width, turnTheta), false);
+		}
+		else {
+			leftMotor.rotate(-convertAngle(radius, width, turnTheta), true);
+			rightMotor.rotate(convertAngle(radius, width, turnTheta), false);
+		}
+
+	}
+	
+	/**
+	 * This method, given distances to travel to, returns the absolute angle of 
+	 * the location relative to the current location of the robot
+	 * @param travelX distance to travel in the x direction
+	 * @param travelY distance to travel in the y direction
+	 */
+	private double absoluteAngle(double travelX, double travelY) {
+		double angle = 0;
+		if (travelX + ERROR > 0) { // account for some error
+			angle = Math.toDegrees(Math.atan(Math.abs(travelX) / Math.abs(travelY)));
+			if (travelY < 0) {
+				angle = 180 - angle;
+			}
+		}
+		else {
+			angle = Math.toDegrees(Math.atan(Math.abs(travelX) / Math.abs(travelY)));
+			if (travelY < 0) {
+				angle = 180 + angle;
+			}
+			else {
+				angle = 360 - angle;
+			}
+		}
+		return angle;
+	}
+	
+	/**
+	 * Length (angular) of a shortest way between two angles. It will be in range [0, 180]. 
+	 * taken from https://stackoverflow.com/questions/7570808/how-do-i-calculate-the-difference-of-two-angle-measures
+	 * @param alpha angle1 (degrees)
+	 * @param beta angle2 (degrees)
+	 */
+	public double distance(double alpha, double beta) {
+		double phi = Math.abs(beta - alpha) % 360; // This is either the distance or 360 - distance
+		double distance = phi > 180 ? 360 - phi : phi;
+		return distance;
 	}
 
 	/**

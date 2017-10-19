5fc3ee778646d2c8320540593f13cddcfd1585e3
Moose <mostafa.saadat@outlook.com>
2017-10-19 18:31:09 -0400

    Use runnable instead of thread

--- src/ca/mcgill/ecse211/localizationlab/Zipline.java
+++ src/ca/mcgill/ecse211/localizationlab/Zipline.java
@@ -2,7 +2,7 @@ package ca.mcgill.ecse211.localizationlab;
 
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 
-public class Zipline extends Thread{
+public class Zipline implements Runnable{
 	
 	EV3LargeRegulatedMotor motor;
 	private final int SPEED = 50;
